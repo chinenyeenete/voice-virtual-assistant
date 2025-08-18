d95b37f8297265b5d053d00bb3afed2c1a409a94
heyyitscandee <189556569+IamChinenye@users.noreply.github.com>
2025-08-18 19:46:03 -0400

    initial commit

---
+++ voice_assistant.py
@@ -0,0 +1,14 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# getting agent ID and elevenlabs api
+AGENT_ID = os.getenv("AGENT_ID")
+API_KEY = os.getenv("API_KEY")
+
+# importing elevenlabs dependencies
+from elevenlabs.client import ElevenLabs
+from elevenlabs.conversational_ai.conversation import Conversation
+from elevenlabs.conversational_ai.default_audio_interface import DefaultAudioInterface
+from elevenlabs.types import ConversationConfig
