72f67146bcba1813559aa2cb97a8c117ed090e5b
heyyitscandee <189556569+IamChinenye@users.noreply.github.com>
2025-08-21 13:25:14 -0400

    added initial prompt

--- voice_assistant.py
+++ voice_assistant.py
@@ -12,3 +12,11 @@ from elevenlabs.client import ElevenLabs
 from elevenlabs.conversational_ai.conversation import Conversation
 from elevenlabs.conversational_ai.default_audio_interface import DefaultAudioInterface
 from elevenlabs.types import ConversationConfig
+
+# Creating first prompt
+user_name = "Chinenye"
+schedule = "Project due at 5:00pm; Date at 7:00pm."
+prompt = f"Hey assistant. Your interlocutor has the following schedule: {schedule}"
+first_message = f"Hello {user_name}, how can I assist you today?"
+
+# Conversation configuration override
